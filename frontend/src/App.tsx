a0ae73efae5c68d4e43612293df886ff6f1308f9
Hebert Paixao <resilire.adm@gmail.com>
2022-07-12 21:16:08 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
-import NotificationButton from "./components/notificationbutton"
+import Header from "./components/header"
+
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Bora Rafael Se espertar</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-    </>
-    )
-  }
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
 
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
 
 export default App
